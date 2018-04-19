b8b3cf3384da6d5229085cb52630f0be8fdd8739
Phil Henault <philhen27@gmail.com>
2018-04-18 22:21:52 -0400

    basic layout need to add fields

--- web_app/weather_app.py
+++ web_app/weather_app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-	return 'WEATHER_APP'
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
     app.secret_key = 'secret12345'
