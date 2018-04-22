8cbb3951fe566041dbb1760397aceeaddfef74be
Phil Henault <philhen27@gmail.com>
2018-04-22 15:10:54 -0400

    current state

---
+++ web_app/forms.py
@@ -0,0 +1,23 @@
+from wtforms import Form, SelectField, SubmitField, validators, ValidationError, DateField
+from wtforms_components import DateTimeField, DateRange
+from datetime import datetime
+
+
+'''cities = [('Vancouver', 'Vancouver'), ('Portland', 'Portland'), ('San Francisco', 'San Francisco'), 
+('Seattle', 'Seattle'), ('Los Angeles', 'Los Angeles'), ('San Diego', 'San Diego'), 
+('Las Vegas', 'Las Vegas'), ('Phoenix', 'Phoenix'), ('Albuquerque', 'Albuquerque'), 
+('Denver', 'Denver'), ('San Antonio', 'San Antonio'), ('Dallas', 'Dallas'), ('Houston', 'Houston'), 
+('Kansas City', 'Kansas City'), ('Minneapolis', 'Minneapolis'), ('Saint Louis', 'Saint Louis'), 
+('Chicago', 'Chicago'), ('Nashville', 'Nashville'), ('Indianapolis', 'Indianapolis'), ('Atlanta', 'Atlanta'),
+ ('Detroit', 'Detroit'), ('Jacksonville', 'Jacksonville'), ('Charlotte', 'Charlotte'), ('Miami', 'Miami'), 
+ ('Pittsburgh', 'Pittsburgh'), ('Toronto', 'Toronto'), ('Philadelphia', 'Philadelphia'), 
+('New York', 'New York'), ('Montreal', 'Montreal'), ('Boston', 'Boston'), ('Beersheba', 'Beersheba'), 
+('Tel Aviv District', 'Tel Aviv District'), ('Eilat', 'Eilat'), ('Haifa', 'Haifa'), 
+('Nahariyya', 'Nahariyya'), ('Jerusalem', 'Jerusalem')]'''
+
+mytest = [('Vancouver', 'Vancouver'), ('Portland', 'Portland'), ('San Francisco', 'San Francisco')]
+
+class InputForm(Form):
+    city = SelectField(label='City', choices=mytest ,coerce=str)
+    date = DateField(label='Date (Y-m-d)', format='%Y-%m-%d', validators=[DateRange(min=datetime(2017, 1, 1),max=datetime(2017, 11, 30))])
+    submit = SubmitField('Submit')
--- web_app/weather_app.py
+++ web_app/weather_app.py
@@ -1,13 +1,25 @@
-from flask import Flask , render_template, flash, redirect, url_for
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from flask import Flask , render_template, flash, redirect, url_for, request
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField
+
+
 from functools import wraps
+from forms import InputForm
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	form = InputForm()
+	#if request.method == 'POST' and form.validate():
+	if request.method == 'POST' and form.validate():
+		print(request.form['city'])
+		print(request.form['date'])
+		return render_template('index.html', form=form)
+	
+	return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.secret_key = 'secret12345'
