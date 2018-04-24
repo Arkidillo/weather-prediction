f109c7e360ec8d5eea629f0c614b63158345b1a5
Phil Henault <philhen27@gmail.com>
2018-04-23 22:59:08 -0400

    most of web app with validation

--- web_app/forms.py
+++ web_app/forms.py
@@ -3,7 +3,7 @@ from wtforms_components import DateTimeField, DateRange
 from datetime import datetime
 
 
-'''cities = [('Vancouver', 'Vancouver'), ('Portland', 'Portland'), ('San Francisco', 'San Francisco'), 
+cities = [('Vancouver', 'Vancouver'), ('Portland', 'Portland'), ('San Francisco', 'San Francisco'), 
 ('Seattle', 'Seattle'), ('Los Angeles', 'Los Angeles'), ('San Diego', 'San Diego'), 
 ('Las Vegas', 'Las Vegas'), ('Phoenix', 'Phoenix'), ('Albuquerque', 'Albuquerque'), 
 ('Denver', 'Denver'), ('San Antonio', 'San Antonio'), ('Dallas', 'Dallas'), ('Houston', 'Houston'), 
@@ -13,11 +13,19 @@ from datetime import datetime
  ('Pittsburgh', 'Pittsburgh'), ('Toronto', 'Toronto'), ('Philadelphia', 'Philadelphia'), 
 ('New York', 'New York'), ('Montreal', 'Montreal'), ('Boston', 'Boston'), ('Beersheba', 'Beersheba'), 
 ('Tel Aviv District', 'Tel Aviv District'), ('Eilat', 'Eilat'), ('Haifa', 'Haifa'), 
-('Nahariyya', 'Nahariyya'), ('Jerusalem', 'Jerusalem')]'''
+('Nahariyya', 'Nahariyya'), ('Jerusalem', 'Jerusalem')]
 
-mytest = [('Vancouver', 'Vancouver'), ('Portland', 'Portland'), ('San Francisco', 'San Francisco')]
+'''mytest = [('Vancouver', 'Vancouver'), ('Portland', 'Portland'), ('San Francisco', 'San Francisco')]
+
+mytest2 = [(0, 'Vancouver'), (1, 'Portland'), (2, 'San Francisco')]
+
+print(type(mytest[0][0]))
+print(type(mytest[0][1]))'''
+
+
+
 
 class InputForm(Form):
-    city = SelectField(label='City', choices=mytest ,coerce=str)
-    date = DateField(label='Date (Y-m-d)', format='%Y-%m-%d', validators=[DateRange(min=datetime(2017, 1, 1),max=datetime(2017, 11, 30))])
+    city = SelectField(label='City', coerce=str, choices=cities)
+    date = DateField(label='Date (Y-m-d)', format='%Y-%m-%d' , validators=[DateRange(min=datetime(2017, 1, 1),max=datetime(2017, 11, 30))])
     submit = SubmitField('Submit')
--- web_app/weather_app.py
+++ web_app/weather_app.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template, flash, redirect, url_for, request
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField
-
+from datetime import datetime, date, timedelta
 
 from functools import wraps
 from forms import InputForm
@@ -8,17 +8,27 @@ from forms import InputForm
 
 app = Flask(__name__)
 
-
+def validate_date(date_text):
+    try:
+        date = datetime.strptime(date_text, '%Y-%m-%d')
+        if date.year != 2017:
+        	return 0 
+    except ValueError:
+        return 0	
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = InputForm()
+
 	#if request.method == 'POST' and form.validate():
-	if request.method == 'POST' and form.validate():
-		print(request.form['city'])
-		print(request.form['date'])
-		return render_template('index.html', form=form)
-	
+	if request.method == 'POST':
+		if validate_date(request.form['date']) != 0:
+		#datetime_object = datetime.strptime(request.form['date'], '%Y-%m-%d')
+			print(request.form['city'])
+			print(request.form['date'])
+			return 'THIS IS A TEST'
+		else:
+			return render_template('errorindex.html',  form=form)	
 	return render_template('index.html', form=form)
 
 if __name__ == '__main__':
